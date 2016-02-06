039ac6647969ade6774c5821533c1c2937f9fd7a
zhanghailiang <zhang.zhanghailiang@huawei.com>
2016-02-06 16:05:57 +0800

    COLO/filter: add each netdev a buffer filter
    For COLO periodic mode, it need to buffer packets that
    sent by VM, and we will not release these packets until
    finish a checkpoint.
    
    Here, we add each netdev a buffer-filter that will be controlled
    by COLO. It is disabled by default, and the packets will not pass
    through these filters. If users don't enable COLO while configure
    qemu, these buffer-filters will not be added.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Yang Hongyang <hongyang.yang@easystack.cn>

--- include/migration/colo.h
+++ include/migration/colo.h
@@ -37,4 +37,6 @@ COLOMode get_colo_mode(void);
 void colo_do_failover(MigrationState *s);
 
 bool colo_shutdown(void);
+void colo_add_buffer_filter(Notifier *notifier, void *data);
+
 #endif
--- stubs/migration-colo.c
+++ stubs/migration-colo.c
@@ -49,3 +49,7 @@ bool colo_shutdown(void)
 {
     return false;
 }
+
+void colo_add_buffer_filter(Notifier *notifier, void *data)
+{
+}
