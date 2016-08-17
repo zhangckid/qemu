6a704ed4ff170f9c8316162bcaea4fc01bfc37dc
Zhang Chen <zhangchen.fnst@cn.fujitsu.com>
2016-08-17 19:55:13 +0800

    net/net: Bugfix for net_fill_rstate()
    When network is busy, we will receive multiple packets
    at one time. In that situation, this bug will lost all
    packets except the first one.
    
    Signed-off-by: Zhang Chen <zhangchen.fnst@cn.fujitsu.com>
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>

--- net/net.c
+++ net/net.c
@@ -1586,9 +1586,8 @@ void net_socket_rs_init(SocketReadState *rs,
 
 /*
  * Returns
- * 0: SocketReadState is not ready
- * 1: SocketReadState is ready
- * otherwise error occurs
+ * 0: success
+ * -1: error occurs
  */
 int net_fill_rstate(SocketReadState *rs, const uint8_t *buf, int size)
 {
@@ -1636,10 +1635,11 @@ int net_fill_rstate(SocketReadState *rs, const uint8_t *buf, int size)
                 if (rs->finalize) {
                     rs->finalize(rs);
                 }
-                return 1;
             }
             break;
         }
     }
+
+    assert(size == 0);
     return 0;
 }
