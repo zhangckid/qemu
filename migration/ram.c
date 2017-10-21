f6f2b994742dc474eec00506b293698833068691
zhanghailiang <zhang.zhanghailiang@huawei.com>
2017-10-21 14:27:39 +0800

    COLO: flush host dirty ram from cache
    Don't need to flush all VM's ram from cache, only
    flush the dirty pages since last checkpoint
    
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>
    Signed-off-by: Zhang Chen <zhangckid@gmail.com>
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- migration/ram.c
+++ migration/ram.c
@@ -3125,6 +3125,7 @@ int colo_init_ram_cache(void)
     }
     ram_state = g_new0(RAMState, 1);
     ram_state->migration_dirty_pages = 0;
+    memory_global_dirty_log_start();
 
     return 0;
 
@@ -3145,10 +3146,12 @@ void colo_release_ram_cache(void)
 {
     RAMBlock *block;
 
+    memory_global_dirty_log_stop();
     QLIST_FOREACH_RCU(block, &ram_list.blocks, next) {
         g_free(block->bmap);
         block->bmap = NULL;
     }
+
     rcu_read_lock();
     QLIST_FOREACH_RCU(block, &ram_list.blocks, next) {
         if (block->colo_cache) {
@@ -3380,6 +3383,13 @@ static void colo_flush_ram_cache(void)
     void *dst_host;
     void *src_host;
     unsigned long offset = 0;
+
+    memory_global_dirty_log_sync();
+    rcu_read_lock();
+    RAMBLOCK_FOREACH_MIGRATABLE(block) {
+        migration_bitmap_sync_range(ram_state, block, 0, block->used_length);
+    }
+    rcu_read_unlock();
 
     trace_colo_flush_ram_cache_begin(ram_state->migration_dirty_pages);
     rcu_read_lock();
